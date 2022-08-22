9fab0fc7dfaf6a2fb5c1ffdfb224eb1ac0bd71a4
mrheio <drg.ov00@gmail.com>
2022-08-22 17:37:27 +0300

    removed useless files

--- state/types.ts
+++
@@ -1,7 +0,0 @@
-export type SetFn<State> = (
-    partial:
-        | State
-        | Partial<State>
-        | ((state: State) => State | Partial<State>),
-    replace?: boolean | undefined
-) => void;
--- tests/helpers.ts
+++
@@ -1,6 +0,0 @@
-import { initializeTestEnvironment } from '@firebase/rules-unit-testing';
-
-export const setup = async () => {
-    const testEnv = await initializeTestEnvironment({});
-    return testEnv;
-};
