82fa442f9dab850dda714081ae5d18a8a33ed5c5
Ciprian <ciprianpitul@gmail.com>
2022-08-22 17:03:00 +0300

    updated next.config, tsconfig
    added aliased absolute paths to various folders

--- hooks/useAuthEffect.ts
+++ hooks/useAuthEffect.ts
@@ -4,18 +4,18 @@ import { authService } from '../api';
 import { AppDispatch, setUser, startSetUser } from '../state';
 
 const useAuthEffect = () => {
-    const dispatch: AppDispatch = useDispatch();
+	const dispatch: AppDispatch = useDispatch();
 
-    useEffect(() => {
-        dispatch(startSetUser());
-        const sub = authService.getUser$().subscribe((val) => {
-            dispatch(setUser(val));
-        });
+	useEffect(() => {
+		dispatch(startSetUser());
+		const sub = authService.getUser$().subscribe(val => {
+			dispatch(setUser(val));
+		});
 
-        return () => {
-            sub.unsubscribe();
-        };
-    }, []);
+		return () => {
+			sub.unsubscribe();
+		};
+	}, []);
 };
 
 export default useAuthEffect;
--- next.config.js
+++ next.config.js
@@ -1,6 +1,42 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const { relative, join } = require('path');
 
-module.exports = nextConfig
+module.exports = {
+	distDir: `${relative(process.cwd(), __dirname)}.next`,
+	reactStrictMode: true,
+	cssModules: true,
+	// env: {
+	// 	exmple: process.env.example,
+	// },
+	// i18n: {
+	// 	locales: ['en-US'],
+	// 	defaultLocale: 'en-US',
+	// },
+	images: {
+		domains: ['firebasestorage.googleapis.com'],
+	},
+	sassOptions: {
+		includePaths: [join(__dirname, 'styles')],
+	},
+	webpack: config => {
+		config.resolve = {
+			...config.resolve,
+			fallback: {
+				fs: false,
+				path: false,
+				os: false,
+			},
+		};
+
+		return config;
+	},
+	async redirects() {
+		return [
+			// {
+			// 	source: '/example',
+			// 	destination: '/',
+			// 	permanent: true,
+			// },
+		];
+	},
+};
