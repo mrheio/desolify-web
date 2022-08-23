91b9a296c8bb5ce010e5e43e62d77898fd99f633
mrheio <drg.ov00@gmail.com>
2022-08-23 20:23:03 +0300

    removed index from Button folder

--- components/index.ts
+++ components/index.ts
@@ -1,3 +1,3 @@
-export { default as Button } from './Button';
+export { default as Button } from './Button/Button';
 export { default as Loading } from './loading';
 export { default as NavLink } from './NavLink';
