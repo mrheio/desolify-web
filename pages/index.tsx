5178922be0453453e7b9a9a69fc4f5573ccb6ff4
Ciprian <ciprianpitul@gmail.com>
2022-08-24 14:17:53 +0300

    added tailwind and index page styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,16 @@
-const Home = () => {
+import { FC } from 'react';
+
+import { joinClasses } from '@utils/basics';
+
+import styles from '@styles/pages/Index.module.scss';
+
+type Props = {};
+
+const Home: FC<Props> = () => {
 	return (
 		<>
-			<section>
+			<section className={joinClasses('grid grid-cols-2 grid-rows-2', styles['section-one'])}>
+				<h1>Home screen</h1>
 				<h1>Home screen</h1>
 			</section>
 		</>
