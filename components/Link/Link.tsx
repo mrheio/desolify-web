ffa55863b1c85f4e67811bec7b6a6d3bb31ec98a
Ciprian <ciprianpitul@gmail.com>
2022-08-24 03:21:52 +0300

    added Link component

--- components/Link/Link.tsx
+++ components/Link/Link.tsx
@@ -3,8 +3,6 @@ import { forwardRef, PropsWithChildren } from 'react';
 
 import { joinClasses } from '@utils/funcs';
 
-import styles from '@components/Button/Button.module.scss';
-
 interface Props extends LinkProps {
 	underline?: boolean;
 	className?: string;
@@ -16,12 +14,7 @@ interface Props extends LinkProps {
 
 const Link = forwardRef<HTMLAnchorElement, PropsWithChildren<Props>>(
 	({ children, underline, fullWidth, className, primary, secondary, naked, href, ...rest }, ref) => {
-		const linkClasses = joinClasses(
-			primary && styles['button--primary'],
-			secondary && styles['button--secondary'],
-			fullWidth && 'full-width',
-			className,
-		);
+		const linkClasses = joinClasses(fullWidth && 'full-width', className);
 
 		return (
 			<NextLink href={href} passHref>
