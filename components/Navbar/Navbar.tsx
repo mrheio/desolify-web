6f2d73d18b528e693056b2f763879bace6c87b31
mrheio <drg.ov00@gmail.com>
2022-08-24 13:32:51 +0300

    moved header content to navbar

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,6 +1,10 @@
 import { FC } from 'react';
 
+import Button from '@components/Button/Button';
+import Link from '@components/Link/Link';
 import NavLink from '@components/Link/NavLink';
+import { faGamepad } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { joinClasses } from '@utils/basics';
 import { URLS } from '@utils/misc';
 import styles from './Navbar.module.scss';
@@ -10,14 +14,25 @@ interface Props {}
 const Navbar: FC<Props> = ({}) => {
 	return (
 		<nav className={joinClasses(styles.navbar, 'fw-semi-bold')}>
-			<ul className={styles['navbar-list']}>
-				<li>
-					<NavLink href={URLS.HOME}>Teams</NavLink>
-				</li>
-				<li>
-					<NavLink href={URLS.GAMES}>Games</NavLink>
-				</li>
-			</ul>
+			<span className={styles['navbar-left']}>
+				<Link href="/">
+					<h1>DESOLIFY</h1>
+				</Link>
+				<ul className={styles['navbar-list']}>
+					<li>
+						<NavLink href={URLS.HOME}>Teams</NavLink>
+					</li>
+					<li>
+						<NavLink href={URLS.GAMES}>Games</NavLink>
+					</li>
+				</ul>
+			</span>
+			<span className={styles['navbar-right']}>
+				<Button shape="rounded">
+					<FontAwesomeIcon icon={faGamepad} />
+					<span className={styles['navbar-button-text']}>Get Started</span>
+				</Button>
+			</span>
 		</nav>
 	);
 };
--- layout/Header/Header.tsx
+++ layout/Header/Header.tsx
@@ -1,30 +1,13 @@
-import { Button, Navbar } from '@components';
-import { faGamepad } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import Link from 'next/link';
+import { Navbar } from '@components';
 
 import { FC } from 'react';
-import styles from './Header.module.scss';
 
 type Props = {};
 
 const Header: FC<Props> = ({}) => {
 	return (
-		<header className={styles.header}>
-			<span className={styles['header-left']}>
-				<a className="link">
-					<Link href="/">
-						<h1>DESOLIFY</h1>
-					</Link>
-				</a>
-				<Navbar />
-			</span>
-			<span className={styles['header-right']}>
-				<Button shape="rounded">
-					<FontAwesomeIcon icon={faGamepad} />
-					<span className={styles['header-button-text']}>Get Started</span>
-				</Button>
-			</span>
+		<header>
+			<Navbar />
 		</header>
 	);
 };
